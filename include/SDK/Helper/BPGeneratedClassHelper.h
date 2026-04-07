f8b7c991137b962670d8bb3007ceddb6c08ecbb2
Okaetsu <111317036+Okaetsu@users.noreply.github.com>
2026-04-07 11:34:04 +0300

    Add CreateInheritedBlueprintClass in BPGeneratedClassHelper

--- include/SDK/Helper/BPGeneratedClassHelper.h
+++ include/SDK/Helper/BPGeneratedClassHelper.h
@@ -6,7 +6,14 @@ namespace RC::Unreal {
     class UObject;
 }
 
+namespace UECustom {
+    class UBlueprintGeneratedClass;
+}
+
 namespace UECustom::BPGeneratedClassHelper {
+    UECustom::UBlueprintGeneratedClass* CreateInheritedBlueprintClass(RC::Unreal::UClass* parentClass, const RC::Unreal::FName& packageName,
+                                                                     const RC::Unreal::FName& assetName, RC::Unreal::EObjectFlags objectFlags);
+
     RC::Unreal::UObject* FindComponentTemplateByName(RC::Unreal::UObject* BPGeneratedClass, RC::Unreal::FName TemplateName);
 
     bool GetGeneratedClassesHierarchy(RC::Unreal::UClass* InClass, RC::Unreal::TArray<RC::Unreal::UObject*>& OutBPGClasses);
