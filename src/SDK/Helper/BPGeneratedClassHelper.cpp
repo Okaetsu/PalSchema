d120873e74d43bb628bee1ebbd2064e716ef23e0
Okaetsu <111317036+Okaetsu@users.noreply.github.com>
2026-04-07 11:34:04 +0300

    Add CreateInheritedBlueprintClass in BPGeneratedClassHelper

--- src/SDK/Helper/BPGeneratedClassHelper.cpp
+++ src/SDK/Helper/BPGeneratedClassHelper.cpp
@@ -1,41 +1,67 @@
+#include "Unreal/CoreUObject/UObject/UnrealType.hpp"
+#include "Unreal/UPackage.hpp"
 #include "SDK/Helper/BPGeneratedClassHelper.h"
 #include "SDK/PalSignatures.h"
+#include "SDK/Classes/Custom/UBlueprintGeneratedClass.h"
 
 using namespace RC;
 using namespace RC::Unreal;
 
-UObject* UECustom::BPGeneratedClassHelper::FindComponentTemplateByName(UObject* BPGeneratedClass, FName TemplateName)
-{
-    using FindComponentTemplateByNameSignature = UObject*(*)(UObject*, const FName&);
-    static void* FunctionPtr = nullptr;
-
-    if (!FunctionPtr)
+namespace UECustom::BPGeneratedClassHelper {
+    UECustom::UBlueprintGeneratedClass* CreateInheritedBlueprintClass(RC::Unreal::UClass* parentClass, const RC::Unreal::FName& packageName,
+        const RC::Unreal::FName& assetName, RC::Unreal::EObjectFlags objectFlags)
     {
-        FunctionPtr = Palworld::SignatureManager::GetSignature("UBlueprintGeneratedClass::FindComponentTemplateByName");
+        auto newPackage = UObjectGlobals::NewObject<UPackage>(nullptr, packageName, static_cast<EObjectFlags>(RF_Public));
+        newPackage->SetRootSet();
+
+        auto newBlueprintClass = UObjectGlobals::NewObject<UECustom::UBlueprintGeneratedClass>(newPackage, assetName, objectFlags);
+        newBlueprintClass->PurgeClass(false);
+        newBlueprintClass->GetClassFlags() |= CLASS_Transient;
+        newBlueprintClass->GetClassWithin() = parentClass->GetClassWithin();
+        newBlueprintClass->GetPropertyLink() = parentClass->GetPropertyLink();
+        newBlueprintClass->SetSuperStruct(parentClass);
+        newBlueprintClass->Bind();
+        newBlueprintClass->StaticLink(true);
+        newBlueprintClass->AssembleReferenceTokenStream(false);
+        newBlueprintClass->SetRootSet();
+
+        return newBlueprintClass;
     }
 
-    if (!FunctionPtr)
+    UObject* UECustom::BPGeneratedClassHelper::FindComponentTemplateByName(UObject* BPGeneratedClass, FName TemplateName)
     {
-        return nullptr;
+        using FindComponentTemplateByNameSignature = UObject * (*)(UObject*, const FName&);
+        static void* FunctionPtr = nullptr;
+
+        if (!FunctionPtr)
+        {
+            FunctionPtr = Palworld::SignatureManager::GetSignature("UBlueprintGeneratedClass::FindComponentTemplateByName");
+        }
+
+        if (!FunctionPtr)
+        {
+            return nullptr;
+        }
+
+        return reinterpret_cast<FindComponentTemplateByNameSignature>(FunctionPtr)(BPGeneratedClass, TemplateName);
     }
 
-    return reinterpret_cast<FindComponentTemplateByNameSignature>(FunctionPtr)(BPGeneratedClass, TemplateName);
-}
-
-bool UECustom::BPGeneratedClassHelper::GetGeneratedClassesHierarchy(UClass* InClass, TArray<UObject*>& OutBPGClasses)
-{
-    using GetGeneratedClassesHierarchySignature = bool(*)(UClass*, TArray<UObject*>&);
-    static void* FunctionPtr = nullptr;
-
-    if (!FunctionPtr)
+    bool UECustom::BPGeneratedClassHelper::GetGeneratedClassesHierarchy(UClass* InClass, TArray<UObject*>& OutBPGClasses)
     {
-        FunctionPtr = Palworld::SignatureManager::GetSignature("UBlueprintGeneratedClass::FindComponentTemplateByName");
+        using GetGeneratedClassesHierarchySignature = bool(*)(UClass*, TArray<UObject*>&);
+        static void* FunctionPtr = nullptr;
+
+        if (!FunctionPtr)
+        {
+            FunctionPtr = Palworld::SignatureManager::GetSignature("UBlueprintGeneratedClass::FindComponentTemplateByName");
+        }
+
+        if (!FunctionPtr)
+        {
+            return false;
+        }
+
+        return reinterpret_cast<GetGeneratedClassesHierarchySignature>(FunctionPtr)(InClass, OutBPGClasses);
     }
 
-    if (!FunctionPtr)
-    {
-        return false;
-    }
-
-    return reinterpret_cast<GetGeneratedClassesHierarchySignature>(FunctionPtr)(InClass, OutBPGClasses);
 }
